586cc1be2ea6b3a814d3ac12e4aa4cb44839bfe1
Jamaika1 <lukaszcz18@wp.pl>
2022-09-11 07:23:44 +0200

    Fix plugin OpenJPH

--- OpenJPH/T1OJPH.cpp
+++ OpenJPH/T1OJPH.cpp
@@ -15,15 +15,22 @@
  *
  */
 #include "simd.h"
-#include "../OJPH/common/ojph_mem.h"
-#include "coding/ojph_block_decoder.h"
-#include "coding/ojph_block_encoder.h"
+#include "ojph_mem.h"
+#include "ojph_block_decoder.h"
+#include "ojph_block_encoder.h"
 #include "ojph_mem.h"
 #include "T1OJPH.h"
 
 #include "grk_includes.h"
 
-const uint8_t grk_cblk_dec_compressed_data_pad_ht = 8;
+
+/*void memset32(uint32_t *dest, uint32_t val, uint32_t count)
+{
+    while (count--)
+        *dest++ = val;
+}*/
+
+const uint8_t grk_cblk_dec_compressed_data_pad_ht = 8U;
 
 namespace ojph
 {
@@ -48,13 +55,13 @@ void T1OJPH::preCompress([[maybe_unused]] grk::CompressBlockExec* block,
 						 [[maybe_unused]] grk::Tile* tile)
 {
 	auto cblk = block->cblk;
-	uint16_t w = (uint16_t)cblk->width();
-	uint16_t h = (uint16_t)cblk->height();
+	uint32_t w = cblk->width();
+	uint32_t h = cblk->height();
 	uint32_t tile_width =
 		(tile->comps + block->compno)->getWindow()->getResWindowBufferHighestStride();
-	auto tileLineAdvance = tile_width - w;
+	auto tileLineAdvance = (int32_t)(tile_width - w);
 	uint32_t cblk_index = 0;
-	int32_t shift = 31 - (block->k_msbs + 1);
+	int32_t shift = (int32_t)(31U - (block->k_msbs + 1U));
 
 	// convert to sign-magnitude
 	if(block->qmfbid == 1)
@@ -66,7 +73,7 @@ void T1OJPH::preCompress([[maybe_unused]] grk::CompressBlockExec* block,
 			{
 				int32_t temp = *tiledp++;
 				int32_t val = temp >= 0 ? temp : -temp;
-				int32_t sign = (int32_t)((temp >= 0) ? 0U : 0x80000000);
+				int32_t sign = (int32_t)((temp >= 0) ? 0 : 0x80000000);
 				int32_t res = sign | (val << shift);
 				unencoded_data[cblk_index] = res;
 				cblk_index++;
@@ -76,14 +83,14 @@ void T1OJPH::preCompress([[maybe_unused]] grk::CompressBlockExec* block,
 	}
 	else
 	{
-		auto tiledp = (float*)block->tiledp;
+		auto tiledp = block->tiledp;
 		for(auto j = 0U; j < h; ++j)
 		{
 			for(auto i = 0U; i < w; ++i)
 			{
-				int32_t t = (int32_t)((float)*tiledp++ * block->inv_step_ht * (float)(1 << shift));
+				int32_t t = *tiledp++ * (int32_t)(block->inv_step_ht) * (1 << shift);
 				int32_t val = t >= 0 ? t : -t;
-				int32_t sign = t >= 0 ? 0 : (int32_t)0x80000000;
+				int32_t sign = (int32_t)((t >= 0) ? 0 : 0x80000000);
 				int32_t res = sign | val;
 				unencoded_data[cblk_index] = res;
 				cblk_index++;
@@ -100,12 +107,12 @@ bool T1OJPH::compress(grk::CompressBlockExec* block)
 	auto cblk = block->cblk;
 	cblk->numbps = 0;
 	// optimization below was causing errors in compressing
-	// if (maximum >= (uint32_t)1<<(31 - (block->k_msbs+1)))
-	uint16_t w = (uint16_t)cblk->width();
-	uint16_t h = (uint16_t)cblk->height();
+	// if (maximum >= (uint32_t)1<<(31U - (block->k_msbs + 1U)))
+	uint32_t w = cblk->width();
+	uint32_t h = cblk->height();
 
 	uint32_t pass_length[2] = {0, 0};
-	ojph::local::ojph_encode_codeblock((uint32_t*)unencoded_data, block->k_msbs, 1, w, h, w,
+	ojph::local::ojph_encode_codeblock((uint32_t*)unencoded_data, (uint32_t)(block->k_msbs), 1, w, h, w,
 									   pass_length, elastic_alloc, next_coded);
 
 	cblk->numPassesTotal = 1;
@@ -122,7 +129,7 @@ bool T1OJPH::decompress(grk::DecompressBlockExec* block)
 	auto cblk = block->cblk;
 	if(!cblk->area())
 		return true;
-	uint16_t stride = (uint16_t)cblk->width();
+	//uint16_t stride = (uint16_t)cblk->width();
 	if(!cblk->seg_buffers.empty())
 	{
 		size_t total_seg_len = 2 * grk_cblk_dec_compressed_data_pad_ht + cblk->getSegBuffersLen();
@@ -130,36 +137,36 @@ bool T1OJPH::decompress(grk::DecompressBlockExec* block)
 		{
 			delete[] coded_data;
 			coded_data = new uint8_t[total_seg_len];
-			coded_data_size = (uint32_t)total_seg_len;
+			coded_data_size = total_seg_len;
 			memset(coded_data, 0, grk_cblk_dec_compressed_data_pad_ht);
 		}
-		memset(coded_data + grk_cblk_dec_compressed_data_pad_ht + cblk->getSegBuffersLen(), 0,
+		memset(coded_data + grk_cblk_dec_compressed_data_pad_ht + (uint8_t)cblk->getSegBuffersLen(), 0,
 			   grk_cblk_dec_compressed_data_pad_ht);
 		uint8_t* actual_coded_data = coded_data + grk_cblk_dec_compressed_data_pad_ht;
-		size_t offset = 0;
+		size_t offset = 0U;
 		for(auto& b : cblk->seg_buffers)
 		{
-			memcpy(actual_coded_data + offset, b->buf, b->len);
+			memcpy(actual_coded_data + offset, b->buf, b->len * sizeof(uint8_t));
 			offset += b->len;
 		}
 
-		size_t num_passes = 0;
+		size_t num_passes = 0U;
 		for(uint32_t i = 0; i < cblk->getNumSegments(); ++i)
 		{
 			auto sgrk = cblk->getSegment(i);
-			num_passes += sgrk->numpasses;
+			num_passes += (uint8_t)sgrk->numpasses;
 		}
 
 		bool rc = false;
 		if(num_passes && offset)
 		{
 			rc = ojph::local::ojph_decode_codeblock(
-				actual_coded_data, (uint32_t*)unencoded_data, block->k_msbs, (uint32_t)num_passes,
-				(uint32_t)offset, 0, cblk->width(), cblk->height(), stride, false);
+				actual_coded_data, (uint32_t*)unencoded_data, (uint32_t)(block->k_msbs), (uint32_t)num_passes,
+				(uint32_t)offset, 0, cblk->width(), cblk->height(), cblk->width(), false);
 		}
 		else
 		{
-			memset(unencoded_data, 0, stride * cblk->height() * sizeof(int32_t));
+			memset(unencoded_data, 0, (uint16_t)cblk->width() * cblk->height() * sizeof(uint32_t));
 		}
 		if(!rc)
 		{
@@ -168,7 +175,7 @@ bool T1OJPH::decompress(grk::DecompressBlockExec* block)
 		}
 	}
 
-	block->tilec->postProcessHT(unencoded_data, block, stride);
+	block->tilec->postProcessHT(unencoded_data, block, (uint16_t)cblk->width());
 
 	return true;
 }
